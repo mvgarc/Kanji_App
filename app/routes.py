a502581ecee4bdf76ca5a5ab9b44a2a75a9525b2
María García <mariav2601@gmail.com>
2024-12-02 14:31:46 -0400

    update: Si la respuesta de la API es exitosa (status 200), procesamos los datos recibidos.

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,16 @@ def index():
 
     if request.method == 'POST':
         kanji = request.form['kanji']
+
+        if kanji:
+            url= f"https://kanjiapi.dev/v1/kanji/{kanji}"
+            response = request.get(url)
+
+            if response.status_code ==200:
+                kanji_data = response.json
+
+                kanji_info = {
+                    "kanji": kanji,
+                    "meanings": kanji_data.get("meanings", ["No meaning found"]),
+                    "kun_readings": kanji_data.get("kun_readings", ["No readings found"]),
+                }
